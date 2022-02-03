ed57c4c747b043b639d1c9de62e8a2b62bf2c73c
ndmetrick <82462717+ndmetrick@users.noreply.github.com>
2022-02-03 14:17:39 -0600

    tests database

--- bin/index.js
+++ bin/index.js
@@ -5,22 +5,16 @@ const seed = require('../seed.js')
 
 const init = async () => {
   try {
-    console.log('I GOT HERE TO THIS')
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
+    if (process.env.SEED === 'true') {
+      await seed()
+    } else {
+      await db.sync()
+    }
+    // start listening (and create a 'server' object representing our server)
+    app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+  } catch (ex) {
+    console.log(ex)
   }
-  //   if (process.env.SEED === 'true') {
-  //     await seed();
-  //   } else {
-  //     await db.sync();
-  //   }
-  //   // start listening (and create a 'server' object representing our server)
-  //   app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
-  //  catch (ex) {
-  //     console.log(ex);
-  //   }
 }
 
 init()
